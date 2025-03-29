cb50cfa2b09a59f42d2ce872e36e3017f2cfceb6
Julien PICART <julienpicart01@gmail.com>
2025-03-29 18:22:25 +0100

    BREAD in progress

--- server/src/modules/member/memberAction.ts
+++ server/src/modules/member/memberAction.ts
@@ -1,2 +1,29 @@
 import type { RequestHandler } from "express";
 import memberRepository from "./memberRepository";
+
+const browse: RequestHandler = async (req, res, next) => {
+  try {
+    const members = await memberRepository.readAll();
+    res.json(members);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const read: RequestHandler = async (req, res, next) => {
+  console.info("req.query", req.query);
+  try {
+    const memberId = Number(req.query.id);
+    console.info("Member:", memberId);
+    const member = await memberRepository.read(memberId);
+    if (!member) {
+      res.sendStatus(404);
+    } else {
+      res.json(member);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default { browse, read };
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import memberRepository from "./modules/member/memberRepository";
+import memberAction from "./modules/member/memberAction";
 
 const router = express.Router();
 
 /* ************************************************************************* */
-// Define Your API Routes Here
+// Members routes
+
+router.get("/api/members", memberAction.browse);
+router.get("/api/members", memberAction.read);
 /* ************************************************************************* */
 
 export default router;
