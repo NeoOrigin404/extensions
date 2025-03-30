14d3e4e36e1f69014ba3b15e995dbb40816d5809
Julien PICART <julienpicart01@gmail.com>
2025-03-30 16:52:29 +0200

    Update comment api message

--- server/src/router.ts
+++ server/src/router.ts
@@ -8,16 +8,16 @@ import memberAction from "./modules/member/memberAction";
 
 // Get all the members
 router.get("/api/members", memberAction.browse);
-// Get a specific member
+// Get a specific member = api/member?id= ?
 router.get("/api/member", memberAction.read);
 
-// Edit a member
+// Edit a member = api/member?id= ?
 router.put("/api/member", memberAction.edit);
 
 // Add a new member
 router.post("/api/members", memberAction.add);
 
-// Delete a member
+// Delete a member = api/member?id= ?
 router.delete("/api/member", memberAction.destroy);
 /* ************************************************************************* */
 
