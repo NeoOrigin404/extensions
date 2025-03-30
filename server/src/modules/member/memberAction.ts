7ff16cdde6c4f86c27865244dc1321dad3629e5e
Julien PICART <julienpicart01@gmail.com>
2025-03-30 16:44:38 +0200

    BREAD member : OK

--- server/src/modules/member/memberAction.ts
+++ server/src/modules/member/memberAction.ts
@@ -12,7 +12,12 @@ const browse: RequestHandler = async (req, res, next) => {
 
 const read: RequestHandler = async (req, res, next) => {
   try {
-    const memberId = Number(req.params.id);
+    const memberId = Number(req.query.id);
+
+    if (!memberId) {
+      res.json({ error: "ID must be a number" });
+    }
+
     const member = await memberRepository.read(memberId);
     if (!member) {
       res.sendStatus(404);
@@ -24,4 +29,55 @@ const read: RequestHandler = async (req, res, next) => {
   }
 };
 
-export default { browse, read };
+const edit: RequestHandler = async (req, res, next) => {
+  try {
+    const member = {
+      id: Number(req.query.id),
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      username: req.body.username,
+      email: req.body.email,
+      hashed_password: req.body.hashed_password,
+      premium: req.body.premium,
+      role: req.body.role,
+    };
+    const affectedRows = await memberRepository.update(member);
+    if (affectedRows === 0) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(204);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+const add: RequestHandler = async (req, res, next) => {
+  try {
+    const member = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      username: req.body.username,
+      email: req.body.email,
+      hashed_password: req.body.hashed_password,
+      premium: req.body.premium,
+      role: req.body.role,
+    };
+    const insertId = await memberRepository.create(member);
+    res.status(201).json({ insertId });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const destroy: RequestHandler = async (req, res, next) => {
+  try {
+    const memberId = Number(req.query.id);
+    await memberRepository.delete(memberId);
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default { browse, read, edit, add, destroy };
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,16 +1,24 @@
 import express from "express";
-import memberAction from "./modules/member/memberAction";
-console.info("Routes loaded: /api/members & /api/members/:id");
 
 const router = express.Router();
 
 /* ************************************************************************* */
 // Members routes
+import memberAction from "./modules/member/memberAction";
 
 // Get all the members
 router.get("/api/members", memberAction.browse);
 // Get a specific member
-router.get("/api/members/:id", memberAction.read);
+router.get("/api/member", memberAction.read);
+
+// Edit a member
+router.put("/api/member", memberAction.edit);
+
+// Add a new member
+router.post("/api/members", memberAction.add);
+
+// Delete a member
+router.delete("/api/member", memberAction.destroy);
 /* ************************************************************************* */
 
 export default router;
